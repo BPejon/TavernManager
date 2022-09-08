85dd382b85408101fdf23453a4b6bde16d65354f
Breno Pejon <brepejon@gmail.com>
2022-09-08 15:39:36 -0300

    Tavern Management System on Spring Boot

---
+++ src/main/java/com/tavernmanager/Tavern/Manager/TavernManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tavernmanager.Tavern.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavernManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavernManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tavernmanager/Tavern/Manager/TavernManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavernmanager.Tavern.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavernManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
