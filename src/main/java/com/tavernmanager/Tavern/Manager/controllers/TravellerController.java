58c62571c1e866bd29a5bb65f721fdf43bc17af8
Breno Pejon <brepejon@gmail.com>
2022-09-12 14:47:08 -0300

    getTraveller, getAllTraveller and createTraveller implemented

--- src/main/java/com/tavernmanager/Tavern/Manager/controllers/TravellerController.java
+++ src/main/java/com/tavernmanager/Tavern/Manager/controllers/TravellerController.java
@@ -1,7 +1,52 @@
 package com.tavernmanager.Tavern.Manager.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.tavernmanager.Tavern.Manager.models.TravellerModel;
+import com.tavernmanager.Tavern.Manager.repositories.TravellerRepository;
+import com.tavernmanager.Tavern.Manager.services.TravellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.persistence.EntityListeners;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/traveller")
+@EntityListeners(AuditingEntityListener.class)
 public class TravellerController {
+
+    @Autowired
+    private TravellerService travellerService;
+
+    @PostMapping
+    public ResponseEntity<Object> createTraveller(@RequestBody TravellerModel traveller){
+        TravellerModel newTraveller = travellerService.createTraveller(traveller);
+
+        //retorna a URI de acesso ao novo objeto
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{name}").buildAndExpand(newTraveller.getName()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping(value = "/{name}")
+    public ResponseEntity<Object> getTraveller(@PathVariable String name){
+        Optional<TravellerModel> traveller = travellerService.getTraveller(name);
+        if(!traveller.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Traveller not found!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(traveller);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TravellerModel>> getAllTraveller(){
+        List<TravellerModel> allTravellers = travellerService.getAllTravellers();
+        return ResponseEntity.status(HttpStatus.OK).body(allTravellers);
+    }
+
+
+
 }
--- src/main/java/com/tavernmanager/Tavern/Manager/services/TravellerService.java
+++ src/main/java/com/tavernmanager/Tavern/Manager/services/TravellerService.java
@@ -1,7 +1,27 @@
 package com.tavernmanager.Tavern.Manager.services;
 
+import com.tavernmanager.Tavern.Manager.models.TravellerModel;
+import com.tavernmanager.Tavern.Manager.repositories.TravellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TravellerService {
+
+    @Autowired
+    private TravellerRepository travellerRepository;
+    public TravellerModel createTraveller(TravellerModel traveller) {
+        return travellerRepository.save(traveller);
+    }
+
+    public Optional<TravellerModel> getTraveller(String name) {
+        return travellerRepository.findById(name);
+    }
+
+    public List<TravellerModel> getAllTravellers() {
+        return travellerRepository.findAll();
+    }
 }
