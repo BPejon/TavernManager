8c889b151d48e78dadbb3da3cafdb59a69af3a7b
Breno Pejon <brepejon@gmail.com>
2022-10-18 12:50:11 -0300

    dialog added

---
+++ tavernFront/src/app/components/views/beverages/dialogs/dialog-delete/dialog-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-delete',
+  templateUrl: './dialog-delete.component.html',
+  styleUrls: ['./dialog-delete.component.css']
+})
+export class DialogDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
