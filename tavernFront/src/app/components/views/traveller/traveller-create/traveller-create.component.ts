e7b3f6b8eea3715b09961af6bf55d17232bf0535
Breno Pejon <brepejon@gmail.com>
2022-10-24 15:18:46 -0300

    traveller create implemented

--- tavernFront/src/app/components/views/traveller/traveller-create/traveller-create.component.ts
+++ tavernFront/src/app/components/views/traveller/traveller-create/traveller-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Traveller } from '../traveller.model';
+import { TravellerService } from '../traveller.service';
 
 @Component({
   selector: 'app-traveller-create',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravellerCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service : TravellerService, private router: Router) { }
+
+  traveller: Traveller = {
+    name: "",
+    province: "",
+    classType: "",
+    level : 0,
+    coins: 0 
+  }
 
   ngOnInit(): void {
+  }
+
+  create(){
+    this.service.create(this.traveller).subscribe(ans =>{
+      this.service.message(`${this.traveller.name} created!`);
+      this.router.navigate(["traveller"]);
+    }, err => {
+      this.service.message("Please, fill all the blanks.");
+    })
+
+  }
+
+  navigateToBeverageRead(){
+    this.router.navigate(["traveller"]);
   }
 
 }
